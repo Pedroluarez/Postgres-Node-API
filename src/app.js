9ddc7291566a9210ab54ae2fcee9f944cf9e528f
Pedroluarez <luarezpeterr@gmail.com>
2023-12-11 01:25:36 +0800

    update the yaml format

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 "use strict";
 
 const express = require("express");
-const app = express();
+const app = express();    
 const config = require("../config");
-const router = require("../src/routes/router"); 
-const swaggerUi = require("swagger-ui-express"); 
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-
-
+const router = require("../src/routes/router");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
   
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.json()); 
-app.use("/api", router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use("/api/v1", router);
 
 module.exports = {
   listen: app.listen(config.app.port, (req, res) => {
     try {
       console.log(`[API] App started and listening on port ${config.app.port}`);
     } catch (error) {
-      res.status(500).json({ error: error.message, stack: process.env.NODE_ENV === "development" ? err.stack : null });
+      res
+        .status(500)
+        .json({
+          error: error.message,
+          stack: process.env.NODE_ENV === "development" ? err.stack : null,
+        });
     }
   }),
 };
--- src/routes/router.js
+++ src/routes/router.js
@@ -5,6 +5,7 @@ const router = Router();
 const authentication = require("../middleware/auth");
 const connectionStatus = require("../controllers/connectionStatus");   
  
+
 router.get("/status",authentication.validateApp, connectionStatus.getStatus);
 router.use("/users", require("./users"));
 router.use("/students", require("./students"));
